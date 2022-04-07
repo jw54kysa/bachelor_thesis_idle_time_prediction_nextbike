02a50d4d30485c1979e303d1052e51eb2838a9fa
jw54kysa <jonathan.weske@gmail.com>
2022-04-07 19:30:31 +0200

    add features in_zone , year

--- scripts/mlpr_remote.py
+++ scripts/mlpr_remote.py
@@ -1,13 +1,16 @@
 import pandas as pd
+from matplotlib import pyplot as plt
 from sklearn.preprocessing import StandardScaler
-from sklearn.metrics import zero_one_loss, accuracy_score
+from sklearn.metrics import r2_score, mean_squared_error
 from sklearn.neural_network import MLPClassifier
+from sklearn.model_selection import train_test_split
 import wandb
 import random
+import seaborn as sns
 
 idle_time_data = pd.read_csv('../data/df_points/df_points_18_21_class.csv')
 
-TargetVariable = ['idle_time_class']
+TargetVariable = ['idle_time']
 Predictors = ['bike_id', 'lat', 'lng', 'temp', 'rain', 'snow', 'dt_start', 'hex_enc', 'start_min', 'month', 'day']
 
 X = idle_time_data[Predictors].values
@@ -17,12 +20,14 @@ PredictorScaler = StandardScaler()
 PredictorScalerFit = PredictorScaler.fit(X)
 X = PredictorScalerFit.transform(X)
 
-from sklearn.model_selection import train_test_split
+TargetScaler = StandardScaler()
+TargetScalerFit = TargetScaler.fit(y)
+y = TargetScalerFit.transform(y)
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, train_size=0.9, shuffle=False)
 
 sweep_configuration = {
-    "project": "MultiLayerPerceptronClassification",
+    "project": "MultiLayerPerceptronRegression",
     "name": "MLPC-sweep-hidden-layer-sizes",
     "metric": {"name": "accuracy", "goal": "maximize"},
     "method": "random",
@@ -44,6 +49,7 @@ sweep_configuration = {
         }
     }
 }
+
 
 def my_train_func():
     wandb.init()
@@ -71,21 +77,24 @@ def my_train_func():
     model.fit(X_train, y_train.ravel())
     y_pred = model.predict(X_test)
 
-    score_training = model.score(X_train, y_train.ravel())
-    score_validation = model.score(X_test, y_test.ravel())
-
-    acc = accuracy_score(y_test.ravel(), y_pred.ravel())
-    loss = zero_one_loss(y_test.ravel(), y_pred.ravel())
+    r2 = r2_score(y_test, y_pred.ravel())
+    mse = mean_squared_error(y_test, y_pred.ravel())
+    print(f"R2: {r2}")
+    print(f"MSE: {mse}")
 
     # wandb.sklearn.plot_feature_importances(model, Predictors)
 
-    wandb.log({"accuracy": acc, "loss": loss})
-    wandb.log({"conf_matrix": wandb.plot.confusion_matrix(y_true=y_test.ravel(), preds=y_pred.ravel())})
+    plt.figure(figsize=(10, 10))
+    reg_plot = sns.regplot(y_test, y_pred, fit_reg=True, scatter_kws={"s": 100})
+
+    wandb.log({"r2": r2, "mse": mse})
+    wandb.log({"reg_plot": reg_plot})
+    # wandb.log({"conf_matrix": wandb.plot.confusion_matrix(y_true=y_test.ravel(), preds=y_pred.ravel())})
     # wandb.log({"feature_imp": wandb.sklearn.plot_feature_importances(model, Predictors)})
-    wandb.log({"score_training": score_training, "score_validation": score_validation})
+    # wandb.log({"score_training": score_training, "score_validation": score_validation})
 
 
 # INIT SWEEP
-sweep_id_rfc = wandb.sweep(sweep_configuration, project="MultiLayerPerceptronClassification")
+sweep_id_rfc = wandb.sweep(sweep_configuration, project="MultiLayerPerceptronRegression")
 # RUN SWEEP
 wandb.agent(sweep_id_rfc, function=my_train_func)
